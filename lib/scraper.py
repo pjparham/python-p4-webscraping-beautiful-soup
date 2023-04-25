76c420ddc941ba8ebb88a24bdfef1d3ab87fc22c
Parker Parham <parkerparham@gmail.com>
2023-04-25 16:48:14 -0400

    Adds notes on scraping

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,10 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.selcet('.heading-financier'))
